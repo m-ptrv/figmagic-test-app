5f7a5e624a03f412f977c0dd8f4b8edea589e3ab
Maxim Petrov <>
2023-10-04 22:02:17 +0400

    Update dependencies version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": [   {
-    name: "@storybook/addon-docs",
-    options: { transcludeMarkdown: true },
-  },
-      "@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": "@storybook/react",
-  core: {
-    builder: "webpack5"
-  }
-};
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    {
+      name: "@storybook/addon-docs",
+      options: { transcludeMarkdown: true },
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
